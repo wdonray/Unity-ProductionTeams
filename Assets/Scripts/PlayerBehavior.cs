c0774eec7c537258b6beb3b7c733eff8a4a2504e
regireed89 <regireed89@gmail.com>
2017-05-26 17:57:13 -0500

    Merge branch 'master' of https://github.com/wdonray/Unity-ProductionTeams into Regi

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -6,7 +6,27 @@ public class PlayerBehavior : MonoBehaviour
 {
 
     private int hp;
-    private int strength;
+    private int attackPower;
+    private float speed;
+    public GameObject bottle;
+    private GameObject b;
+    public void ThrowBottle()
+    {
+        b = Instantiate(bottle, transform, false);
+        b.transform.SetParent(null);
+        b.GetComponent<Rigidbody>().velocity += transform.TransformDirection(0, 0, 25);
+
+    }
+
+    public void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+            ThrowBottle();
+
+        b.transform.Rotate(0, 90 * Time.deltaTime, 0);
+
+    }
 
 
 }
