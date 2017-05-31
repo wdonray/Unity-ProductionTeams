b77d4153770f52d7fdcbdee551170000c31805a7
Donray Williams <w.donray@yahoo.com>
2017-05-31 17:30:27 -0500

    Lots of working work, BASE GAME

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -1,34 +1,95 @@
 ﻿using UnityEngine;
 using UnityEngine.AI;
 
+[RequireComponent(typeof(Rigidbody))]
 public class EnemyBehavior : MonoBehaviour
 {
+    private float _attackCd;
+    private float _attackTimer = 2.0f;
+    public int Damage;
+
+    public int Health;
+
+    [SerializeField] private MinionCop _minion;
+
     private NavMeshAgent _nav;
 
-    [Range(1, 10)] public int TargetRange;
+    [SerializeField] private GameObject _player;
 
     [SerializeField] private GameObject _target;
 
-    [SerializeField] private GameObject _player;
+    [HideInInspector] public Tower ATower;
+
+    private float _attackRange = 2f;
+
+    [Range(1, 10)] public int PlayerRange;
+
+    public MinionCop Minion
+    {
+        get { return _minion; }
+    }
+
+    private void Awake()
+    {
+        _minion = ScriptableObject.CreateInstance<MinionCop>();
+        ATower = ScriptableObject.CreateInstance<Tower>();
+        _attackCd = _attackTimer;
+        //Time.timeScale = 10;
+    }
 
     // Use this for initialization
     private void Start()
     {
         _nav = GetComponent<NavMeshAgent>();
         _nav.SetDestination(_target.transform.position);
+        _target = GameObject.FindWithTag("Target");
+        _player = GameObject.FindWithTag("Player");
+        Health = _minion.Health;
+        Damage = _minion.Damage;
+        _attackRange = _nav.stoppingDistance;
     }
 
-    // Update is called once per frame
     private void Update()
     {
-        if (Vector3.Distance(this.transform.position, _player.transform.position) < TargetRange)
+        var inPlayerRange = Vector3.Distance(transform.position, _player.transform.position) < PlayerRange;
+        var inTowerRange = Vector3.Distance(transform.position, _target.transform.position) < _attackRange;
+        var agentstopped = _nav.isStopped;
+        if (agentstopped)
+            GetComponent<MeshRenderer>().material.color = Color.black;
+        else
+            GetComponent<MeshRenderer>().material.color = Color.white;
+
+        if (inPlayerRange) //chase player
         {
             _nav.SetDestination(_player.transform.position);
         }
-        else if (Vector3.Distance(this.transform.position, _target.transform.position) < TargetRange)
+        else
         {
-            _nav.SetDestination(_target.transform.position);
-            Destroy(this);
+            //if a tower is in range
+            if (inTowerRange)
+            {
+                _nav.isStopped = true;
+                if (_attackTimer <= 0)
+                {
+                    _minion.DoDamage(ATower);
+                    _attackTimer = _attackCd;
+                }
+                else
+                {
+                    var curColor = GetComponent<MeshRenderer>().material.color;
+                    GetComponent<MeshRenderer>().material.color =
+                        Color.Lerp(curColor, Color.red, _attackTimer / _attackCd);
+                    _attackTimer -= Time.deltaTime;
+                }
+            }
+            else
+            {
+                _nav.isStopped = false;
+                //we aren't chasing player so find a tower
+                _nav.SetDestination(_target.transform.position);
+            }
         }
+        Health = _minion.Health;
+        Damage = _minion.Damage;
     }
 }
