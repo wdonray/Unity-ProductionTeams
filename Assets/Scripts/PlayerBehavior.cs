823ea0c4c7e375a6647b24055d1ac3a645fd5d46
regireed89 <regireed89@gmail.com>
2017-06-02 15:52:07 -0500

    behaviors

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -1,31 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PlayerBehavior : MonoBehaviour, IDamageable, IDamager
+public class PlayerBehavior : MonoBehaviour, IDamageable
 {
+
+    private GameObject bottle;
+    public GameObject bottlePrefab;
+
+    private int _playerHealth = 100;
+
     
-    public int hp = 100;
-    private int attackPower = 10;
-    public GameObject bottle;
-    private GameObject b;
-    public void ThrowBottle()
+    public int PlayerHealth
     {
-        b = Instantiate(bottle, transform, false);
-        b.transform.SetParent(null);
-        b.GetComponent<Rigidbody>().velocity += transform.TransformDirection(0, 0, 25);
-        Destroy(b, 10);
+        get { return _playerHealth; }
+        set { _playerHealth = value; }
     }
 
 
     public void TakeDamage(int amount)
     {
-        throw new System.NotImplementedException();
+        PlayerHealth -= amount;
     }
+   
 
-    public void DoDamage(IDamageable defender)
+    public void ThrowBottle()
     {
-        throw new System.NotImplementedException();
+        bottle = Instantiate(bottlePrefab, transform, false);
+        bottle.transform.SetParent(null);
+        bottle.GetComponent<Rigidbody>().velocity += transform.TransformDirection(0, 0, 25);
+        Destroy(bottle, 10);
     }
 
     public void Dead()
@@ -33,57 +35,15 @@ public class PlayerBehavior : MonoBehaviour, IDamageable, IDamager
         gameObject.GetComponent<Material>().color = Color.red;
     }
 
-    Ray cameraRay; // The ray that is cast from the camera to the mouse position
-    RaycastHit cameraRayHit; //RaycastHit cameraRayHit; // The object that the ray hits
 
-    public float theta;
     public void Update()
     {
-       
+        if(PlayerHealth == 0)
+            Dead();
 
-        //top down only
-        var worldPoint = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 13.5f));
-       // transform.LookAt(worldPoint);
-        if (Input.GetMouseButtonDown(0))
-        {
-            var go = Instantiate(prefab, worldPoint, Quaternion.identity);
-
-
-
-            Vector3 targetPos = new Vector3(go.transform.position.x, (go.transform.position.y - go.transform.position.y), go.transform.position.z);
-            gameObject.transform.LookAt(targetPos);
-
-                  
-
-            //Vector3 dis = transform.position - targetPos;
-            //theta = Vector3.Dot(transform.forward, targetPos);
-            //transform.Rotate(Vector3.up, Mathf.Abs(theta) * Mathf.Rad2Deg);
-
-
-            //theta = Vector3.Dot(transform.forward,  go.transform.position);
-            //transform.Rotate(Vector3.up, Mathf.Abs(theta) * Mathf.Rad2Deg);
-        }
-
-        //// Cast a ray from the camera to the mouse cursor
-        //cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        //// If the ray strikes an object...  
-        //if (Physics.Raycast(cameraRay, out cameraRayHit))
-        //{
-        //    // ...and if that object is the ground...
-        //    if (cameraRayHit.transform.tag == "Ground")
-        //    {
-        //        // ...make the cube rotate (only on the Y axis) to face the ray hit's position 
-        //        Vector3 targetposition = new Vector3(Input.mousePosition.x, this.transform.position.y, Input.mousePosition.z);
-        //        this.transform.LookAt(targetposition);
-          //  }
-       // }
-
-
-
-
+        if(Input.GetMouseButtonDown(0))
+            ThrowBottle();
     }
 
-
-    public GameObject prefab;
+  
 }
