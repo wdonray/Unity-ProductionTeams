7efa2a73b80aaf39e143f22c61bde578a5763e83
regireed89 <regireed89@gmail.com>
2017-06-07 02:21:43 -0500

    go

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -4,48 +4,44 @@ using UnityEngine.UI;
 public class PlayerBehavior : MonoBehaviour, IDamageable
 {
 
-	private GameObject bottle;
-	public GameObject bottlePrefab;
-	private int _playerHealth = 100;
+    private GameObject bottle;
+    public GameObject bottlePrefab;
+    private int _playerHealth = 100;
 	public Text playerHp;
+    
+    public int PlayerHealth
+    {
+        get { return _playerHealth; }
+        set { _playerHealth = value; }
+    }
 
-	public int PlayerHealth
-	{
-		get { return _playerHealth; }
-		set { _playerHealth = value; }
-	}
+    public void TakeDamage(int amount)
+    {
+        PlayerHealth -= amount;
+    }
 
-
-	public void TakeDamage(int amount)
-	{
-		PlayerHealth -= amount;
-	}
-
-
-	public void ThrowBottle()
-	{
-		bottle = Instantiate(bottlePrefab, transform);
-		bottle.GetComponent<Rigidbody>().velocity += transform.TransformDirection(0, 0, 25);
-		Destroy(bottle, 5);
-	}
+    public void ThrowBottle()
+    {
+		bottle = Instantiate (bottlePrefab, transform);
+        bottle.GetComponent<Rigidbody>().velocity += transform.TransformDirection(0, 0, 25);
+        Destroy(bottle, 5);
+    }
 
 
 
 	public void Start()
 	{
-		playerHp = GameObject.FindGameObjectWithTag("PlayerHP").GetComponent<Text>();
+		playerHp = GameObject.FindGameObjectWithTag("Player").GetComponent<Text>();
 	}
 
-	public void Update()
-	{
-		if (PlayerHealth <= 0)
-			return;
+    public void Update()
+    {
+        if(PlayerHealth <= 0)
+            return;
 
-		if (Input.GetMouseButtonDown(0))
-			ThrowBottle();
+        if(Input.GetMouseButtonDown(0))
+            ThrowBottle();
 
-		playerHp.text = PlayerHealth.ToString();
-	}
-
-
+		playerHp.text = PlayerHealth.ToString ();
+    }
 }
