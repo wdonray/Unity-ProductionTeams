7e85837f5907db9914c292070583c0c033ea7e7f
Donray Williams <w.donray@yahoo.com>
2017-06-09 18:00:42 -0500

    freak

---
+++ Assets/Scripts/ChangeCan.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ChangeCan : MonoBehaviour
+{
+    public Sprite CrushedCan, NormalCan;
+    private bool _isCrushed = false;
+
+    public void ChangeCanClick()
+    {
+        _isCrushed = !_isCrushed;
+        GetComponent<Image>().sprite = (_isCrushed) ? CrushedCan : NormalCan;
+    }
+}
