7268fa09997e6de49028bfd3323f91fa5a493dc0
Matthew Williamson <chuwilliamson@gmail.com>
2017-06-15 15:27:53 -0500

    cigarette pack animation

---
+++ Assets/Scripts/TestCigaretteBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCigaretteBehaviour : MonoBehaviour
+{
+    public AnimationCurve ac;
+    IEnumerator TweenScale()
+    {
+        var timer = 0f;
+        var startScale = transform.localScale;
+        while (timer < 1f)
+        {
+            timer += Time.deltaTime;
+            float value = ac.Evaluate(timer);
+            var newscale = new Vector3(1, 1, 1) * value;
+            transform.localScale = startScale + newscale;
+            yield return null;
+        }
+    }
+
+    public void OpenPack()
+    {
+        
+    }
+
+    public void ClosePack()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(TweenScale());
+    }
+
+    private void OnDisable()
+    {
+        transform.localScale = new Vector3(1, 1, 1);
+    }
+}
