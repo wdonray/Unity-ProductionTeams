65a3fd554b24adaa52036c07b87c49a68995e15b
Matthew Williamson <chuwilliamson@gmail.com>
2017-06-15 18:00:01 -0500

    cigarettes

--- Assets/Scripts/PauseGame.cs
+++ Assets/Scripts/PauseGame.cs
@@ -6,13 +6,16 @@ using UnityEngine.UI;
 public class PauseGame : MonoBehaviour
 {
     public Transform ControlsMenu;
+    public Transform PauseCanvas;
     public Transform PauseMenu;
+    public Transform PauseBackground;
     public GameObject Player;
     public Sprite NormalCan;
     public List<Image> Buttons = new List<Image>();
-
+    Rigidbody playerRigidbody;
     private void Start()
     {
+        playerRigidbody = Player.GetComponent<Rigidbody>();
         PauseMenu.gameObject.SetActive(false);
         ControlsMenu.gameObject.SetActive(false); 
     }
@@ -22,28 +25,25 @@ public class PauseGame : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Escape))
             Pause();
     }
-
+    public bool paused = false;
     public void Pause()
     {
-        foreach(var button in Buttons)
+        paused = !paused;
+        if (paused)
         {
-            if(button.sprite != NormalCan)
-                button.sprite = NormalCan;
-        }
-        var rb = Player.GetComponent<Rigidbody>();
-        if (PauseMenu.gameObject.activeInHierarchy == false)
-        {
+            playerRigidbody.isKinematic = true;
             PauseMenu.gameObject.SetActive(true);
+            PauseCanvas.gameObject.SetActive(true);
             ControlsMenu.gameObject.SetActive(false);
+            PauseBackground.gameObject.SetActive(true);
             Time.timeScale = 0;
-            rb.isKinematic = true;
-            PauseMenu.gameObject.SetActive(true);
         }
-        else
+        else if (!paused)
         {
             Time.timeScale = 1;
-            rb.isKinematic = false; 
-            PauseMenu.gameObject.SetActive(false);
+            playerRigidbody.isKinematic = false;
+            PauseBackground.gameObject.SetActive(false);
+            PauseMenu.GetComponentInChildren<TestCigaretteBehaviour>().ClosePack();
         }
     }
 
@@ -56,18 +56,20 @@ public class PauseGame : MonoBehaviour
     {
         if (open == true)
         {
-            Buttons[2].sprite = NormalCan;
-            Buttons[3].sprite = NormalCan;
+            //Buttons[2].sprite = NormalCan;
+            //Buttons[3].sprite = NormalCan;
             ControlsMenu.gameObject.SetActive(true);
             PauseMenu.gameObject.SetActive(false);
+            PauseBackground.gameObject.SetActive(false);
             //Debug.Log("open");
         }
         else if (open == false)
         {
-            Buttons[3].sprite = NormalCan;
-            Buttons[2].sprite = NormalCan;
+            //Buttons[3].sprite = NormalCan;
+            //Buttons[2].sprite = NormalCan;
             PauseMenu.gameObject.SetActive(true);
             ControlsMenu.gameObject.SetActive(false);
+            PauseBackground.gameObject.SetActive(true);
             //Debug.Log("closed");
         }
     }
--- Assets/Scripts/TestCigaretteBehaviour.cs
+++ Assets/Scripts/TestCigaretteBehaviour.cs
@@ -1,41 +1,62 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TestCigaretteBehaviour : MonoBehaviour
 {
     public AnimationCurve ac;
+    public Animator anim;
+    public Text PausedText;
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
     IEnumerator TweenScale()
     {
         var timer = 0f;
         var startScale = transform.localScale;
-        while (timer < 1f)
+        while (timer < .5f)
         {
-            timer += Time.deltaTime;
-            float value = ac.Evaluate(timer);
+            timer += Time.fixedDeltaTime;
+            var value = ac.Evaluate(timer);
             var newscale = new Vector3(1, 1, 1) * value;
             transform.localScale = startScale + newscale;
             yield return null;
         }
+        PausedText.enabled = true;
     }
 
-    public void OpenPack()
+    IEnumerator TweenScaleBack()
     {
-        
-    }
-
-    public void ClosePack()
-    {
-        
+        var timer = 0f;
+        var totalTime = .5f;
+        var startScale = transform.localScale;
+        PausedText.enabled = false;
+        while(timer < totalTime)
+        {
+            timer += Time.fixedDeltaTime;
+            transform.localScale = Vector3.Lerp(startScale, new Vector3(1, 1, 1), timer/totalTime);
+            yield return null;
+        }
+        transform.localScale = new Vector3(1, 1, 1);
+        gameObject.SetActive(false);
     }
 
     private void OnEnable()
     {
+        anim.SetTrigger("open");
         StartCoroutine(TweenScale());
     }
-
     private void OnDisable()
     {
         transform.localScale = new Vector3(1, 1, 1);
     }
+    public void ClosePack()
+    {
+        
+        anim.SetTrigger("close");
+        StartCoroutine(TweenScaleBack());
+    }
+    
 }
